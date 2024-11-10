533d1340f2e53f526dc5da26aaa32587ac939df8
sharathkumar-tm <sharath.kumar@trainingmug.com>
2024-11-10 20:41:10 +0530

    abstract methods

---
+++ src/com/trainginmug/abstraction/ProductService.java
@@ -0,0 +1,8 @@
+package com.trainginmug.abstraction;
+
+public abstract class ProductService {
+
+    //Business Methods
+
+    abstract void addProduct(Product product);//Business logic to store the product in to the database
+}
---
+++ src/com/trainginmug/abstraction/ProductServiceImpl2.java
@@ -0,0 +1,10 @@
+package com.trainginmug.abstraction;
+
+public class ProductServiceImpl2 extends ProductService{
+
+    @Override
+    void addProduct(Product product){
+        System.out.println("Product is successfully added to Oracle Database!!");
+    }
+
+}
